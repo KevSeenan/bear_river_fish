d2d7c0dbb969841892fd4ef16e7a1d097d13e36d
KevSeenan <kev.seenan@gmail.com>
2019-07-23 18:07:27 +0100

    Commit for adding fish and no of fish in river

--- river.rb
+++ river.rb
@@ -4,7 +4,16 @@ class River
 
   def initialize(river_name)
     @river_name = river_name
-    # @fish = fish
+    @fish_population = []
   end
+
+  def fish_count()
+    return @fish_population.count
+  end
+
+  def add_fish(fish)
+    @fish_population << fish
+  end
+
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -1,7 +1,7 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../river.rb')
-# require_relative('../fish.rb')
+require_relative('../fish.rb')
 
 class RiverTest < MiniTest::Test
 
@@ -13,11 +13,26 @@ class RiverTest < MiniTest::Test
     @fish3 = Fish.new("Nemo")
     @fish4 = Fish.new("Dory")
 
-    @fish = [@fish1, @fish2, @fish3, @fish4]
+    @fish_population = [@fish1, @fish2, @fish3, @fish4]
 
   end
 
   def test_get_river_name()
     assert_equal("Amazon", @river.river_name)
   end
+
+  def test_get_fish_count()
+    assert_equal(0, @river.fish_count)
+  end
+
+  def test_get_fish_population()
+
+    @river.add_fish(@fish1)
+    @river.add_fish(@fish2)
+    @river.add_fish(@fish3)
+    @river.add_fish(@fish4)
+
+    assert_equal(4, @river.fish_count)
+  end
+
 end
