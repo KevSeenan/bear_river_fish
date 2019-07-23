4fd618695cf35134d0b3b9bb63e608a001989a2f
KevSeenan <kev.seenan@gmail.com>
2019-07-23 18:14:31 +0100

    Commit for bear name and type

---
+++ bears.rb
@@ -0,0 +1,10 @@
+class Bear
+
+  attr_reader :bear_name, :bear_type
+
+  def initialize(bear_name, bear_type)
+    @bear_name = bear_name
+    @bear_type = bear_type
+  end
+  
+end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -1,9 +1,19 @@
 require('minitest/autorun')
 require('minitest/rg')
-require_relative('../bear.rb')
+require_relative('../bears.rb')
 
-class BusTest < MiniTest::Test
+class BearTest < MiniTest::Test
 
   def setup()
+    @bear = Bear.new("Baloo", "Grizzly")
   end
+
+  def test_get_bear_name()
+    assert_equal("Baloo", @bear.bear_name)
+  end
+
+  def test_get_bear_type()
+    assert_equal("Grizzly", @bear.bear_type)
+  end
+
 end
